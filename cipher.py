99f5cce02ee7bc682e2e5492fb2003492e5a0810
latrunculorum <ericchambers19@gmail.com>
2016-01-06 18:35:37 -0500

    changed some stuff

--- cipher.py
+++ cipher.py
@@ -1,5 +1,19 @@
+from collections import OrderedDict
+
 alph = 'abcdefghijklmnopqrstuvwxyz'
-keyword = input('please enter the keyword: ')
-cipherbet = alph+keyword
-cipherbet = sorted(set(cipherbet))
-print(cipherbet)
+keyword = raw_input('please enter the keyword: ')
+
+
+cipherbet = keyword+alph
+
+for i in alph:
+
+
+
+##CipherAlphabet = "".join(OrderedDict.fromkeys(sorted(cipherbet)))
+
+
+
+
+print cipherbet
+print CipherAlphabet
